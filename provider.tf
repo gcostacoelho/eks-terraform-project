a0fa623164bdc45c145af2efd7f5c8f9304f651f
Gustavo Costa <gcostacoelho2003@gmail.com>
2025-01-23 20:45:13 -0300

    feat: add a backend configuration

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.84.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "gustavo-studies-s3"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
