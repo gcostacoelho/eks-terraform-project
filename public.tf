9ea1bb42a14e70b0fca4c3686fd0cebc68b80839
Gustavo Costa <gcostacoelho2003@gmail.com>
2025-01-23 22:26:58 -0300

    feat: create a public subnet and get region dynamically

---
+++ public.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "eks-pub-1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block_vpc, 8, 1)
+  availability_zone       = "${data.aws_region.current.name}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                     = "gustavo-studies-pub-1a"
+    "kubernetes.io/role/elb" = 1
+  }
+}
+
+resource "aws_subnet" "eks-pub-1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block_vpc, 8, 2)
+  availability_zone       = "${data.aws_region.current.name}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                     = "gustavo-studies-pub-1b"
+    "kubernetes.io/role/elb" = 1
+  }
+}
