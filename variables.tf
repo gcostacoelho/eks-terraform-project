9b73250f67389c36499eb81e01ab2c296d3c8f4e
Gustavo Costa <gcostacoelho2003@gmail.com>
2025-01-23 22:25:20 -0300

    feat: create variables file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# VPC Variables
+
+variable "cidr_block_vpc" {
+  type        = string
+  description = "Networking CIDR block to be used for the VPC"
+
+  validation {
+    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}($|/(16|24))$", var.cidr_block_vpc))
+    error_message = "This format is not valid"
+  }
+}
