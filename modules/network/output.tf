f59b323426a78ef243608e28e5d8e3a4e4f5476d
Gustavo Costa <gcostacoelho2003@gmail.com>
2025-01-27 20:28:13 -0300

    feat: created a output

---
+++ modules/network/output.tf
@@ -0,0 +1,19 @@
+output "subnet_pub_1a" {
+  value = aws_subnet.eks_pub_1a.id
+}
+
+output "subnet_pub_1b" {
+  value = aws_subnet.eks_pub_1b.id
+}
+
+output "subnet_priv_1a" {
+  value = aws_subnet.eks_priv_1a.id
+}
+
+output "subnet_priv_1b" {
+  value = aws_subnet.eks_priv_1b.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.eks_vpc.id
+}
