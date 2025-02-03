b3a437d398afddc3918b6a184284e211dc61499f
Gustavo Costa <gcostacoelho2003@gmail.com>
2025-02-03 11:07:40 -0300

    feat: create service accounts

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -2,6 +2,14 @@ output "cluster_name" {
   value = aws_eks_cluster.cluster_main.id
 }
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.cluster_main.endpoint
+}
+
+output "cluster_ca_cert" {
+  value = aws_eks_cluster.cluster_main.certificate_authority[0].data
+}
+
 output "oidc" {
   value = aws_eks_cluster.cluster_main.identity[0].oidc[0].issuer
 }
--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.84.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.1"
+    }
   }
 
   backend "s3" {
@@ -15,4 +20,14 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_ca_cert)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
